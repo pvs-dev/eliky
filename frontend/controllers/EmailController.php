e20ad4d4dd7da3282e2d627f628d0432774a7543
vladimir <voxxa90@gmail.com>
2020-11-25 15:13:12 +0200

    fix

--- frontend/controllers/EmailController.php
+++ frontend/controllers/EmailController.php
@@ -5,6 +5,7 @@ namespace frontend\controllers;
 use Yii;
 use common\models\Email;
 use common\models\search\EmailSearch;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
